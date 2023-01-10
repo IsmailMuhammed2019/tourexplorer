45f2dbeec0bdd9b0f98f7d417953e12a09408622
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-10 16:49:43 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div>App</div>
-  )
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
+function App() {
+  return <h2>Tours Project Setup</h2>
 }
 
 export default App
